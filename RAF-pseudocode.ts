b5a24f1a087a6bf604372a3fe6b037231bac1723
GoatInAHat <bennett.g.vernon@gmail.com>
2026-02-09 21:16:30 -0600

    pseudocode done

--- RAF-pseudocode.ts
+++ RAF-pseudocode.ts
@@ -7,27 +7,26 @@ type LiteLLMModel = any
 type cachedInput = any
 type ModelIO = cachedInput | string // can be cached for cloud models that support input token caching, can also be a promise to wait on
 
-interface childPlan {
-    purpose: string
-    parentPlan: parentPlan
-    dependsOn: childPlan[] // pointers to siblings that this agent will wait for the output of (if the siblings in this list are successful)
-}
-
-interface parentPlan { // placeholder for recursive step plan data type. Includes information about task breakdown, which planned children are independent (async) or dependent on the output of a sibling or siblings, and control flow between dependent siblings
-    parentPurpose: string // current node's purpose
-    plannedChildren: childPlan[]
+interface childNodePlan {
+    context: ModelIO
+    name: string
+    dependsOn: string[] // list of names of siblings that this agent will wait for the output of (if the siblings in this list are successful)
 }
 
 interface nodeResult {
-
+    name: string
+    success?: boolean
+    upstreamContext?: string // context for summarization of running and info to pass to child nodes
+    childExecutions: {[key: string]: nodeResult}
 }
 
 //Independent variables to test via benchmarks
 const BASE_CASE_JURY_CONFIG = {  max_temp: 0.5, min_temp: 0.1, size: 10, agents: [], context: "Is this a base case or recursive case?", options: [{}, {}], format: {} } // agents: [] is a placeholder, example of the current set of agents being tested. Can have 1 or more agents. Test impacts of average compute level and compute level diversity. 
 const BASE_CASE_CONSORTIUM_CONFIG = { max_temp: 0.5, min_temp: 0.1, size: 10, agents: [], context: "Design base case agent", format: {} } // format for this will be agent config ready to be called, including tooling, starting context, success condition, and other custom required JSON output fields. Can even force strings to fit regex.
 const BASE_CASE_CONSORTIUM_JURY_CONFIG = { max_temp: 0.5, min_temp: 0.1, size: 10, agents: [], context: "Vote on the best base case agent", options: [{}, {}], format: {} }
-const BASE_CASE_ANALYSIS_CONSORTIUM_CONFIG = { max_temp: 0.5, min_temp: 0.1, size: 10, agents: [], context: "Analyze on this excecution", format: { "success" : "boolean", "info" : "string" } } // also just a placeholder to conceptually outline whats happening
-const BASE_CASE_ANALYSIS_CONSORTIUM_JURY_CONFIG = { max_temp: 0.5, min_temp: 0.1, size: 10, agents: [], context: "Vote on the best analysis", options: [{}, {}], format: {} }
+
+const EXEC_ANALYSIS_CONSORTIUM_CONFIG = { max_temp: 0.5, min_temp: 0.1, size: 10, agents: [], context: "Analyze on this excecution", format: { "success" : "boolean", "info" : "string" } } // also just a placeholder to conceptually outline whats happening
+const EXEC_ANALYSIS_CONSORTIUM_JURY_CONFIG = { max_temp: 0.5, min_temp: 0.1, size: 10, agents: [], context: "Vote on the best analysis", options: [{}, {}], format: {} }
 
 const RECURSIVE_CASE_PLAN_CONSORTIUM_CONFIG = { max_temp: 0.5, min_temp: 0.1, size: 10, agents: [], context: "Come up with a plan for decomposing this recursive case into next-smallest cases", format: {} } // format will be parentPlan
 const RECURSIVE_CASE_PLAN_CONCAT_CONSORTIUM_CONFIG = { max_temp: 0.5, min_temp: 0.1, size: 10, agents: [], context: "Perform a union operation on all the proposed plans into a final list of proposed plans. Merge any plans that are identical or nearly identical. If any two parts of a plan conflict/are contradictory, then they are seperate plans and should not be merged", format: {} } // format is list of parentPlan
@@ -40,25 +39,36 @@ class RafNode {
     // The result of the RAF node execution, and any relevant details about the success or failure for debugging purposes and context any nodes that are waiting for this node to complete.
     result: nodeResult
     state: "initialized" | "running" | "completed"
-    children: RafNode[]
-    context: string
+    children: {[key: string]: RafNode}
+    context: ModelIO
+    parent: RafNode | undefined // undefined if node is root
+    dependencies: Promise<{promise: Promise<nodeResult>, node: RafNode[]}[]>
+    resolveDependencies!: (value: {promise: Promise<nodeResult>, node: RafNode[]}[] | PromiseLike<{promise: Promise<nodeResult>, node: RafNode[]}[]>) => void;
+    name: string
 
-    constructor(context: string) {
+    constructor(context: ModelIO, parent?: RafNode, name?: string) {
         this.context = context
+        this.parent = parent
+        this.name = name || "root"
         this.state = "initialized"
-        this.result = { baseCase: false, success: false, resultAnalysis: "" }
-        this.children = []
+        this.result = { name: this.name, childExecutions: {}}
+        this.children = {}
+        this.dependencies = parent ? new Promise<{promise: Promise<nodeResult>, node: RafNode[]}[]>((resolve) => { // wait for dependencies to be configured by parent
+            this.resolveDependencies = resolve;
+        }) : Promise.resolve([]) // if root then resolve dependencies to nothing immediately
     }
 
     async base_case_vote(): Promise<boolean> {
-        return await new AgentJury(BASE_CASE_JURY_CONFIG).do_voting()
+        const BaseCaseJury = new AgentJury(BASE_CASE_JURY_CONFIG)
+        await BaseCaseJury.set_context(this.context)
+        return await BaseCaseJury.do_voting()
     }
 
     async base_case(): Promise<nodeResult> {
         const AgentDesignConsortium = new AgentConsortium(BASE_CASE_CONSORTIUM_CONFIG) // A group of agents that, given a task, context, list of tools, and output format, generate a list of potential base case executor agent designs. A base case excecutor agent design is a full agent starting context window, including system prompt with task spec context, tooling, success condition, and output format, which includes all context nescessary to start excecution immediately. It should have a very focused context window, with very clear instructions, only the tools it will use, and should excecute only one step. Multiple steps should be split between sequential sibling recursive nodes.
         const AgentDesignJury = new AgentJury(BASE_CASE_CONSORTIUM_JURY_CONFIG)
-        const AnalysisConsortium = new AgentConsortium(BASE_CASE_ANALYSIS_CONSORTIUM_CONFIG)
-        const AnalysisJury = new AgentJury(BASE_CASE_ANALYSIS_CONSORTIUM_JURY_CONFIG)
+        const AnalysisConsortium = new AgentConsortium(EXEC_ANALYSIS_CONSORTIUM_CONFIG)
+        const AnalysisJury = new AgentJury(EXEC_ANALYSIS_CONSORTIUM_JURY_CONFIG)
 
         await AgentDesignJury.set_options(await AgentDesignConsortium.call())
 
@@ -72,7 +82,7 @@ class RafNode {
 
         const analysis = await AnalysisJury.do_voting()
 
-        return { baseCase: true, success: analysis.success, resultAnalysis: analysis.info }
+        return { name: this.name, success: analysis.success, upstreamContext: analysis.info, childExecutions: {} }
     }
 
     async recursive_case(): Promise<nodeResult> {
@@ -81,33 +91,67 @@ class RafNode {
         const RecursiveCasePlanConcatJury = new AgentJury(RECURSIVE_CASE_PLAN_CONCAT_JURY_CONFIG)
         const RecursiveCasePlanJury = new AgentJury(RECURSIVE_CASE_PLAN_JURY_CONFIG)
 
-        const plans: parentPlan[] = await RecursiveCasePlanConsortium.call()
+        const AnalysisConsortium = new AgentConsortium(EXEC_ANALYSIS_CONSORTIUM_CONFIG)
+        const AnalysisJury = new AgentJury(EXEC_ANALYSIS_CONSORTIUM_JURY_CONFIG)
 
         // check for circular sibling dependencies and throw out plans that have circular dependence
-        const plansWithoutCircularDeps = plans.filter(plan => !this.hasCircularDependency(plan));
+        const plans: childNodePlan[][] = (await RecursiveCasePlanConsortium.call()).filter(plan => !this.has_circular_dependency(plan))
 
         RecursiveCasePlanConcatConsortium.set_context(plans)
 
-        const planConcatenations: parentPlan[][] = await RecursiveCasePlanConcatConsortium.call()
+        const planConcatenations: childNodePlan[][] = await RecursiveCasePlanConcatConsortium.call()
 
         await RecursiveCasePlanConcatJury.set_options(planConcatenations)
         const bestPlanConcatenation = await RecursiveCasePlanConcatJury.do_voting()
 
         await RecursiveCasePlanJury.set_options(bestPlanConcatenation)
-        const bestPlan: parentPlan = await RecursiveCasePlanJury.do_voting()
+        const plan: childNodePlan[] = await RecursiveCasePlanJury.do_voting()
 
+        const planDependencies: {[key: string]: string[]} = {}
 
+        for (const childPlan of plan) {
+            this.children[childPlan.name] = new RafNode(childPlan.context, this, childPlan.name)
+            planDependencies[childPlan.name] = childPlan.dependsOn
+        }
 
-        // TODO: start running independent children asynchronously, then run dependent siblings in the right order
+        const childExecutionPromises: {[key: string]: Promise<nodeResult>} = {}
+
+        for (const childName in this.children) {
+            childExecutionPromises[childName] = this.children[childName].call()
+        }
+
+        for (const childName in this.children) {
+            this.children[childName].set_dependencies(planDependencies[childName].map(depName => ({ promise: childExecutionPromises[depName], node: [this.children[depName]] })))
+        }
+
+        const results: {[key: string]: nodeResult} = Object.fromEntries(
+            (await Promise.all(Object.values(childExecutionPromises))).map(childResult => [childResult.name, childResult])
+        )
+
+        AnalysisConsortium.set_context(results)
+        AnalysisJury.set_options(await AnalysisConsortium.call())
+        const analysis = await AnalysisJury.do_voting()
+
+        return { name: this.name, success: analysis.success, upstreamContext: analysis.info, childExecutions: results }
     }
-    hasCircularDependency(plan: parentPlan): boolean {
-        return false // TODO: figure out this algorithm
+
+    set_dependencies(dependencies: {promise: Promise<nodeResult>, node: RafNode[]}[]) {
+        this.resolveDependencies(dependencies)
     }
 
-    async call(agents: Agent[]): Promise<nodeResult> {
+    has_circular_dependency(plan: childNodePlan[]): boolean { // detect circular sibling dependencies
+        return false // placeholder, implementation unnescessary for pseudocode
+    }
+
+    async call(): Promise<nodeResult> {
+        const dependencies = await this.dependencies // wait for dependencies to be configured by parent
+        const dependentSiblingExecutions = await Promise.all(dependencies.map(dep => dep.promise)) // wait for all sibling nodes that this node depends on to finish excecution
+        
+        this.context = "Dependency excecutions: " + JSON.stringify(dependentSiblingExecutions) + "\n" + this.context // add context from siblings that this node depends on to this.context
+
         this.state = "running"
 
-        return await this.base_case_vote() ? await this.base_case() : await this.recursive_case()
+        return await this.base_case_vote() ? this.base_case() : this.recursive_case()
     }
 }
 
